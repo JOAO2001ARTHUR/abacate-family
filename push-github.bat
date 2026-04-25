4a1f9f9ecc487a5c85dcf5f55f961b17fd724ee8
Desenvolvedor <dev@abacate.family>
2026-04-25 02:22:26 -0300

    fix: params type for Next.js 16

---
+++ push-github.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\LENOVO\Desktop\Antigravity Oficial\Abacate Family"
+"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/JOAO2001ARTHUR/abacate-family.git
+"C:\Program Files\Git\cmd\git.exe" push -u origin master
+pause
